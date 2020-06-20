0c5baea743b4931f266135ccafa5d0977d117324
Lyon Liang <lyon.liang12@gmail.com>
2020-06-20 16:19:27 +0800

    [New Feat]: 去除常用时间段的提示

--- packages/grafana-ui/src/components/TimePicker/TimePickerContent/TimePickerContent.tsx
+++ packages/grafana-ui/src/components/TimePicker/TimePickerContent/TimePickerContent.tsx
@@ -255,18 +255,6 @@ const EmptyRecentList = memo(() => {
 
   return (
     <div className={styles.container}>
-      <div>
-        <span>
-          It looks like you haven't used this timer picker before. As soon as you enter some time intervals, recently
-          used intervals will appear here.
-        </span>
-      </div>
-      <div>
-        <a className={styles.link} href="https://grafana.com/docs/grafana/latest/reference/timerange/" target="_new">
-          Read the documentation
-        </a>
-        <span> to find out more about how to enter custom time ranges.</span>
-      </div>
     </div>
   );
 });
